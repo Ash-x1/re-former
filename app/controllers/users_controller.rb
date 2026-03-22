fb6317b7a07bdea21c36cb20da2c33a7fca8ea3c
Ash-x1 <ash91p@proton.me>
2026-03-22 15:24:39 +0100

    Edit the controller and routes to add update users

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,12 +1,21 @@
 class UsersController < ApplicationController
+  before_action :set_user, only: [:show, :edit, :update]
+
+  def index
+    @users = User.all 
+  end
+
+  def show
+  end
+
   def new
     @user = User.new
   end
 
   def create
     # @user = User.new(username: params[:username], email: params[:email], password: params[:password])
-    @user = User.new(params[:user])
-    # @user = User.new(user_params)
+    # @user = User.new(params[:user])
+    @user = User.new(user_params)
     
     if @user.save
       redirect_to new_user_path
@@ -14,10 +23,30 @@ class UsersController < ApplicationController
       render :new, status: :unprocessable_entity
     end
   end
+  
+  def edit
+  end
 
-  # def user_params
-    # user_params = params.expect(user: [:username, :email, :password])
+  def update
+    if @user.update(user_params)
+      redirect_to @user 
+    else 
+      render :edit
+    end
+  end
+
+  # def destroy
+    # @user.destroy
+    # redirect_to users_path
   # end
+
+  def set_user
+    @user = User.find(params[:id])
+  end
+
+  def user_params
+    params.require(:user).permit(:username, :email, :password)
+  end
 
   def save
   end
